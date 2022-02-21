191a6a698d62be874a361cac3999ffb19e7d043e
eze <eze@pop-os.localdomain>
2022-02-21 10:25:03 +0100

    chore: innitial maven setup project

---
+++ galley-grub-application/src/main/java/edu/poniperro/App.java
@@ -0,0 +1,13 @@
+package edu.poniperro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
