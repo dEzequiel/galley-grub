8de628c34df816f29aef49328556fa767fd3a188
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-22 02:04:02 +0100

    feat(getItem): construction! method to return identical object if its exist else it creats it

--- galley-grub-application/src/main/java/edu/poniperro/galleygrub/items/ItemFactory.java
+++ galley-grub-application/src/main/java/edu/poniperro/galleygrub/items/ItemFactory.java
@@ -1,13 +1,20 @@
 package edu.poniperro.galleygrub.items;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 
 public class ItemFactory {
 
-    private Map<String, Double> itemMap = new HashMap<>();
+    private static Set<Item> itemMap = new HashSet<>();
 
     public ItemFactory(){};
 
-    
+    public static Item getItem(String name, double price) {
+        Optional<Item> desiredItem = itemMap.stream()
+                                                    .filter(i -> i.isRegular() && Objects.equals(i.name(), name))
+    }
+
+
 }
