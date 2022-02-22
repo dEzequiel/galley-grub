21a6f9a76ef7289b1fa093e9755a0551967aa030
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-22 17:47:49 +0100

    test(undersrtandNotRegularItemBehaviour): add @Before and test for not regular items, that means items with extras

--- galley-grub-application/src/test/java/edu/poniperro/galleygrub/items/ItemFactoryTest.java
+++ galley-grub-application/src/test/java/edu/poniperro/galleygrub/items/ItemFactoryTest.java
@@ -2,13 +2,20 @@ package edu.poniperro.galleygrub.items;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.poniperro.galleygrub.order.Order;
 
 public class ItemFactoryTest {
 
+    @Before
+    public void setuItemFactory() {
+        ItemFactory.clear();
+        assertEquals(0, ItemFactory.size());
+    }
     @Test
     public void understandItemFactoryBehaviour() {
 
@@ -24,8 +31,20 @@ public class ItemFactoryTest {
         assertEquals(kpatty, ItemFactory.getItem("Krabby Patty", 1.25));
         assertEquals(coral, ItemFactory.getItem("Coral Bits", 1.00));
 
-        assertEquals(2, ItemFactory.size());
-        ItemFactory.clear();
-        assertEquals(0, ItemFactory.size());
+    }
+
+    @Test
+    public void understandNotRegularItemBehaviour() {
+        Item kpatty = ItemFactory.getItem("Krabby Patty", 1.25, "cheese");
+        assertNotNull(kpatty);
+
+        assertEquals(1, ItemFactory.size());
+
+        assertEquals(kpatty, ItemFactory.getItem("Krabby Patty", 1.25, "cheese"));
+        assertEquals("Krabby Patty w/ cheese....1.25$ + 0.25$", kpatty.toString());
+
+        assertNotNull(ItemFactory.getItem("Krabby Patty", 1.25, "cheese"));
+
+
     }
 }
