a70aadd68359548ecf17459b24b375edfaee7562
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-22 17:38:58 +0100

    test(understandItemFactoryBehaviour): add test for checking if items factory is not empty and  it returns object inside set

--- galley-grub-application/src/test/java/edu/poniperro/galleygrub/items/ItemFactoryTest.java
+++ galley-grub-application/src/test/java/edu/poniperro/galleygrub/items/ItemFactoryTest.java
@@ -1,5 +1,8 @@
 package edu.poniperro.galleygrub.items;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 import edu.poniperro.galleygrub.order.Order;
@@ -8,11 +11,21 @@ public class ItemFactoryTest {
 
     @Test
     public void understandItemFactoryBehaviour() {
-        Order order= new Order();
 
-        order.addItem("Krabby Patty", 1.25);
-        order.addItem("Coral Bits", 1.00);
+        Item kpatty = ItemFactory.getItem("Krabby Patty", 1.25);
+        assertNotNull(kpatty);
 
-        
+        assertEquals("Krabby Patty....1.25$", kpatty.toString());
+        assertEquals(1, ItemFactory.size());
+
+        Item coral = ItemFactory.getItem("Coral Bits", 1.00);
+        assertNotNull(kpatty);
+
+        assertEquals(kpatty, ItemFactory.getItem("Krabby Patty", 1.25));
+        assertEquals(coral, ItemFactory.getItem("Coral Bits", 1.00));
+
+        assertEquals(2, ItemFactory.size());
+        ItemFactory.clear();
+        assertEquals(0, ItemFactory.size());
     }
 }
