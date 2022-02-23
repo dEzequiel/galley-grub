09c1874b5a99499ded27491fff03cf386f57579f
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-23 02:57:31 +0100

    test(checkItemsPriceSum): add test to validate Item objects prices sum

--- galley-grub-application/src/test/java/edu/poniperro/galleygrub/items/OrderTest.java
+++ galley-grub-application/src/test/java/edu/poniperro/galleygrub/items/OrderTest.java
@@ -12,10 +12,10 @@ public class OrderTest {
     @Test
     public void orderElements() {
 
-        Order order= new Order();
+        Order order = new Order();
 
-        order.addItem("Krabby Patty", 1.25);
-        order.addItem("Coral Bits", 1.00);
+        order.addItem("Krabby Patty", 0.00);
+        order.addItem("Coral Bits", 0.00);
 
         assertEquals(2, order.size());
 
@@ -23,5 +23,18 @@ public class OrderTest {
 
     }
 
+    @Test
+    public void checkItemsPriceSum() {
+        
+        Order order = new Order();
+
+        order.addItem("Krabby Patty", 1.25);
+        order.addItem("Coral Bits", 1.00);
+        order.addItem("Kelp Rings", 1.5);
+        order.addItem("Golden Loaf", 2.0);
+        order.addItem("Seafoam Soda", 1.0);
+
+        assertEquals(6.75, order.getTotal(), 0);
+    }
 
 }
