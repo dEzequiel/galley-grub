c13cbfd6be3ff314f09b51999330433117b204e5
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-23 23:51:50 +0100

    feat(Ticket): add interface methods

--- galley-grub-application/src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
+++ galley-grub-application/src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
@@ -1,9 +1,12 @@
 package edu.poniperro.galleygrub.receipt;
 
-import edu.poniperro.galleygrub.items.Comanda;
+import edu.poniperro.galleygrub.extras.Extra;
+import edu.poniperro.galleygrub.order.Comanda;
 
 public interface Ticket {
     Comanda getOrder();
     Double total();
     void print();
+    void setChain(Extra extraType);
+    Extra getChain();
 }
